aad2118a03369a42797cab08a72ac761f2423466
D1mastaSPAL <su66071n@mail.ru>
2024-12-10 19:55:57 +0300

    first commit

---
+++ Lesson5.1/Book.cs
@@ -0,0 +1,56 @@
+﻿namespace Lesson5._1
+{
+    internal class Book
+    {
+        private string _title;
+        private string _author;
+        private int _pages;
+
+        public Book() : this("Unknown", "Unknown", 0)
+        {
+        }
+        public Book(string title, string author, int pages)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+        }
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+            }
+        }
+        public int Pages
+        {
+            get
+            {
+                return _pages;
+            }
+            set
+            {
+                _pages = value;
+            }
+        }
+        public void GetDescription()
+        {
+            Console.Write($"Название: {Title}, Автор: {Author}, Страниц: {Pages}");
+        }
+    }
+}
---
+++ Lesson5.1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lesson5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book objBook1 = new Book();
+            objBook1.GetDescription();
+            Console.Write("\n");
+            try
+            {
+                Book objBook2 = new Book();
+                Console.Write("Введите название книги" + "\n");
+                objBook2.Title = Console.ReadLine();
+
+                if (objBook2.Title == "")
+                {
+                    Console.WriteLine("Вы ничего не ввели!");
+                    throw new ArgumentNullException(nameof(objBook2.Title));
+                }
+
+                Console.Write("Введите имя автора" + "\n");
+                objBook2.Author = Console.ReadLine();
+
+                if (objBook2.Author == "")
+                {
+                    Console.WriteLine("Вы ничего не ввели!");
+                    throw new ArgumentNullException(nameof(objBook2.Author));
+                }
+
+                Console.Write("Введите количество страниц" + "\n");
+                objBook2.Pages = int.Parse(Console.ReadLine());
+                if (objBook2.Pages < 0 && objBook2.Pages < int.MaxValue)
+                {
+                    Console.WriteLine("Книг с таким количеством страницы - нет!");
+                    throw new ArgumentOutOfRangeException(nameof(objBook2.Pages));
+                }
+                objBook2.GetDescription();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка! Вы не ввели количество страниц!");
+            }
+            finally
+            {
+                Console.WriteLine("\n" + "Конец программы");
+            }
+        }
+    }
+}
